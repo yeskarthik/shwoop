ff2495e1a206f8f7816b1b605c5166eacefa43f5
lavanyaj09 <34148958+lavanyaj09@users.noreply.github.com>
2020-10-30 20:48:29 -0700

    first push

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello World!";
+<!-- Your form -->
+<form name="form" id="my-form">
+
+	<!-- Required field email -->
+	<input type="text" name="email" data-rule="required|email"/>
+
+	<input type="submit" value="Submit"/>
+
+</form>
+
+<!-- Append the validator JS script -->
+<script type="text/javascript" src="js-form-validator.js"></script>
+
+<script>
+	// Init validator with standart settings
+	new Validator(document.querySelector('#my-form'), function (err, res) {
+		return res;
+	});
+</script>
